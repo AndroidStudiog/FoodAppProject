52935aa028e07055605a2696a724199e6f4f1338
AndroidStudiog <bitm-f19-098@superior.edu.pk>
2022-03-14 15:09:43 +0500

    project

--- app/src/main/java/com/example/foodappproject/LoginActivity.java
+++ app/src/main/java/com/example/foodappproject/LoginActivity.java
@@ -2,13 +2,38 @@ package com.example.foodappproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+    Button signIn;
+    TextView signIn_register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        signIn=(Button) findViewById(R.id.btn_signIn);
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(LoginActivity.this,MainActivity.class);
+                startActivity(intent);
+                Toast.makeText(LoginActivity.this, "Login is Successfully.", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        signIn_register=(TextView) findViewById(R.id.txt_sign_in_register);
+        signIn_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(LoginActivity.this,RegistrationActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
