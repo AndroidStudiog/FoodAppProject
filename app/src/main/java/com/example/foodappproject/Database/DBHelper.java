6627640dc253fef2d2202b5cd7098514e5b2ec25
AndroidStudiog <bitm-f19-098@superior.edu.pk>
2022-03-14 15:23:29 +0500

    project

--- app/src/main/java/com/example/foodappproject/Database/DBHelper.java
+++ app/src/main/java/com/example/foodappproject/Database/DBHelper.java
@@ -1,4 +1,24 @@
 package com.example.foodappproject.Database;
 
-public class DBHelper {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DBHelper extends SQLiteOpenHelper {
+    public DBHelper(@Nullable Context context) {
+        super(context,"Registration",null,1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase DB1) {
+        DB1.execSQL("create table UserTable(id integer primary key,Name Text,Phone Text,Email Text,Password Text)");
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase DB2, int i, int i1) {
+        DB2.execSQL("drop table if exists UserTable");
+    }
 }
