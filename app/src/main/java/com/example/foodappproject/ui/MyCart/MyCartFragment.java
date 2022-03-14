e4bc04475324aee3c6f0c7e251ff6730a445e5b1
AndroidStudiog <bitm-f19-098@superior.edu.pk>
2022-03-14 14:34:15 +0500

    project

--- app/src/main/java/com/example/foodappproject/ui/MyCart/MyCartFragment.java
+++ app/src/main/java/com/example/foodappproject/ui/MyCart/MyCartFragment.java
@@ -1,4 +1,4 @@
-package com.example.foodappproject;
+package com.example.foodappproject.ui.MyCart;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.example.foodappproject.R;
 
 public class MyCartFragment extends Fragment {
 
