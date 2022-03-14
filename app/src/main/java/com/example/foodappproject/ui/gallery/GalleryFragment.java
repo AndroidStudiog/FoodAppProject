330441ea6e16fe688695199c0f1e3dcc12848f98
AndroidStudiog <bitm-f19-098@superior.edu.pk>
2022-03-14 12:56:10 +0500

    project

--- app/src/main/java/com/example/foodappproject/ui/gallery/GalleryFragment.java
+++ app/src/main/java/com/example/foodappproject/ui/gallery/GalleryFragment.java
@@ -4,34 +4,16 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
 
-import com.example.foodappproject.databinding.FragmentGalleryBinding;
+import com.example.foodappproject.R;
 
 public class GalleryFragment extends Fragment {
 
-    private FragmentGalleryBinding binding;
-
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        GalleryViewModel galleryViewModel =
-                new ViewModelProvider(this).get(GalleryViewModel.class);
-
-        binding = FragmentGalleryBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
-
-        final TextView textView = binding.textGallery;
-        galleryViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_gallery, container, false);
         return root;
-    }
-
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        binding = null;
     }
 }
