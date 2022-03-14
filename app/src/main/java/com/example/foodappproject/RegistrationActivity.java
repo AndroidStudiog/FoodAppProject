25f3d8bfb21913f817c5fdd89e63a96fb71d420b
AndroidStudiog <bitm-f19-098@superior.edu.pk>
2022-03-14 15:00:33 +0500

    project

--- app/src/main/java/com/example/foodappproject/RegistrationActivity.java
+++ app/src/main/java/com/example/foodappproject/RegistrationActivity.java
@@ -2,13 +2,38 @@ package com.example.foodappproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class RegistrationActivity extends AppCompatActivity {
+    Button register;
+    TextView register_sign_in;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+        register=(Button) findViewById(R.id.btn_register);
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(RegistrationActivity.this,LoginActivity.class);
+                startActivity(intent);
+                Toast.makeText(RegistrationActivity.this, "Account is created.", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        register_sign_in=(TextView) findViewById(R.id.txt_register_sign_in);
+        register_sign_in.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(RegistrationActivity.this,LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
