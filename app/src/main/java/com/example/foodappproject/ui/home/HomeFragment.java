cc6b3017dfbeaa77d53aac114d708a38f6aaca3d
AndroidStudiog <bitm-f19-098@superior.edu.pk>
2022-03-15 19:30:22 +0500

    project

--- app/src/main/java/com/example/foodappproject/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/foodappproject/ui/home/HomeFragment.java
@@ -9,15 +9,20 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.foodappproject.R;
 import com.example.foodappproject.databinding.FragmentHomeBinding;
 
 public class HomeFragment extends Fragment {
+    RecyclerView recycleVew;
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        View root = inflater.inflate(R.layout.fragment_home, container, false);
-        return root;
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        recycleVew=(RecyclerView) view.findViewById(R.id.home_hor_rec);
+
+        return view;
     }
 }
