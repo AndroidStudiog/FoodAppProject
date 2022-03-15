2261e70a1cf5febf0bf815ada87aab398ece55cc
AndroidStudiog <bitm-f19-098@superior.edu.pk>
2022-03-15 19:26:33 +0500

    project

--- app/src/main/java/com/example/foodappproject/adapters/HomeHorAdapter.java
+++ app/src/main/java/com/example/foodappproject/adapters/HomeHorAdapter.java
@@ -1,4 +1,56 @@
 package com.example.foodappproject.adapters;
 
-public class HomeHorAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.foodappproject.R;
+import com.example.foodappproject.models.HomeHorModel;
+
+import java.util.ArrayList;
+
+public class HomeHorAdapter extends RecyclerView.Adapter<HomeHorAdapter.ViewHolder> {
+    ArrayList<HomeHorModel> list;
+    Context context;
+
+    public HomeHorAdapter(ArrayList<HomeHorModel> list, Context context) {
+        this.list = list;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view= LayoutInflater.from(context).inflate(R.layout.home_horizontal_items,parent,false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        HomeHorModel mdl=list.get(position);
+        holder.image.setImageResource(mdl.getImage());
+        holder.text.setText(mdl.getName());
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        ImageView image;
+        TextView text;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            image=(ImageView) itemView.findViewById(R.id.hor_img);
+            text=(TextView) itemView.findViewById(R.id.hor_text);
+        }
+    }
 }
