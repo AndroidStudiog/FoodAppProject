3cfbccbf6c6d4285cd3ddbe1dc4628c2a2eba691
AndroidStudiog <bitm-f19-098@superior.edu.pk>
2022-03-15 19:34:44 +0500

    project

--- app/src/main/java/com/example/foodappproject/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/foodappproject/ui/home/HomeFragment.java
@@ -9,10 +9,15 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.foodappproject.R;
+import com.example.foodappproject.adapters.HomeHorAdapter;
 import com.example.foodappproject.databinding.FragmentHomeBinding;
+import com.example.foodappproject.models.HomeHorModel;
+
+import java.util.ArrayList;
 
 public class HomeFragment extends Fragment {
     RecyclerView recycleVew;
@@ -22,6 +27,20 @@ public class HomeFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
 
         recycleVew=(RecyclerView) view.findViewById(R.id.home_hor_rec);
+
+        ArrayList<HomeHorModel> list=new ArrayList<>();
+
+        list.add(new HomeHorModel(R.drawable.food1,"Burger"));
+        list.add(new HomeHorModel(R.drawable.food1,"Burger"));
+        list.add(new HomeHorModel(R.drawable.food1,"Burger"));
+        list.add(new HomeHorModel(R.drawable.food1,"Burger"));
+        list.add(new HomeHorModel(R.drawable.food1,"Burger"));
+
+        HomeHorAdapter adapter=new HomeHorAdapter(list,getContext());
+        recycleVew.setAdapter(adapter);
+
+        LinearLayoutManager linearLayoutManager=new LinearLayoutManager(getContext(),LinearLayoutManager.HORIZONTAL,false);
+        recycleVew.setLayoutManager(linearLayoutManager);
 
         return view;
     }
