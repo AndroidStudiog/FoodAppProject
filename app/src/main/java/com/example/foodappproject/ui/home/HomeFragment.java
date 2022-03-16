873e8cb57bc43fb725b989ab054e8be417717a09
AndroidStudiog <bitm-f19-098@superior.edu.pk>
2022-03-17 01:38:29 +0500

    project

--- app/src/main/java/com/example/foodappproject/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/foodappproject/ui/home/HomeFragment.java
@@ -14,33 +14,57 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.foodappproject.R;
 import com.example.foodappproject.adapters.HomeHorAdapter;
+import com.example.foodappproject.adapters.HomeVerAdapter;
 import com.example.foodappproject.databinding.FragmentHomeBinding;
 import com.example.foodappproject.models.HomeHorModel;
+import com.example.foodappproject.models.HomeVerModel;
 
 import java.util.ArrayList;
 
 public class HomeFragment extends Fragment {
-    RecyclerView recycleVew;
+    RecyclerView hor_RecycleVew, ver_RecycleVew;
 
-    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_home, container, false);
 
-        recycleVew=(RecyclerView) view.findViewById(R.id.home_hor_rec);
+        //////////Horizontal RecycleView
 
-        ArrayList<HomeHorModel> list=new ArrayList<>();
+        hor_RecycleVew = (RecyclerView) view.findViewById(R.id.home_hor_rec);
 
-        list.add(new HomeHorModel(R.drawable.pizza,"Pizza"));
-        list.add(new HomeHorModel(R.drawable.pizza,"Pizza"));
-        list.add(new HomeHorModel(R.drawable.pizza,"Pizza"));
-        list.add(new HomeHorModel(R.drawable.pizza,"Pizza"));
-        list.add(new HomeHorModel(R.drawable.pizza,"Pizza"));
+        ArrayList<HomeHorModel> list1 = new ArrayList<>();
 
-        HomeHorAdapter adapter=new HomeHorAdapter(list,getContext());
-        recycleVew.setAdapter(adapter);
+        list1.add(new HomeHorModel(R.drawable.pizza, "Pizza"));
+        list1.add(new HomeHorModel(R.drawable.pizza, "Pizza"));
+        list1.add(new HomeHorModel(R.drawable.pizza, "Pizza"));
+        list1.add(new HomeHorModel(R.drawable.pizza, "Pizza"));
+        list1.add(new HomeHorModel(R.drawable.pizza, "Pizza"));
 
-        LinearLayoutManager linearLayoutManager=new LinearLayoutManager(getContext(),LinearLayoutManager.HORIZONTAL,false);
-        recycleVew.setLayoutManager(linearLayoutManager);
+        HomeHorAdapter adapter1 = new HomeHorAdapter(list1, getContext());
+        hor_RecycleVew.setAdapter(adapter1);
+
+        LinearLayoutManager linearLayoutManager1 = new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false);
+        hor_RecycleVew.setLayoutManager(linearLayoutManager1);
+
+        //////////Vertical RecycleView
+
+        ver_RecycleVew = (RecyclerView) view.findViewById(R.id.home_ver_rec);
+
+        ArrayList<HomeVerModel> list2 = new ArrayList<>();
+
+        list2.add(new HomeVerModel(R.drawable.food1, "Burger", "10:00 - 23:00", "5.0", "Min - $60"));
+        list2.add(new HomeVerModel(R.drawable.food1, "Burger", "10:00 - 23:00", "5.0", "Min - $60"));
+        list2.add(new HomeVerModel(R.drawable.food1, "Burger", "10:00 - 23:00", "5.0", "Min - $60"));
+        list2.add(new HomeVerModel(R.drawable.food1, "Burger", "10:00 - 23:00", "5.0", "Min - $60"));
+        list2.add(new HomeVerModel(R.drawable.food1, "Burger", "10:00 - 23:00", "5.0", "Min - $60"));
+
+        HomeVerAdapter adapter2 = new HomeVerAdapter(list2, getContext());
+        ver_RecycleVew.setAdapter(adapter2);
+
+        LinearLayoutManager linearLayoutManager2 = new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false);
+        ver_RecycleVew.setLayoutManager(linearLayoutManager2);
 
         return view;
     }
