56b9b533dad71d218cbbcb4e7530752b7ee55fc3
AndroidStudiog <bitm-f19-098@superior.edu.pk>
2022-03-29 08:27:58 +0500

    project

--- app/src/main/java/com/example/foodappproject/adapters/UpdateVerticalRec.java
+++ app/src/main/java/com/example/foodappproject/adapters/UpdateVerticalRec.java
@@ -1,4 +1,9 @@
 package com.example.foodappproject.adapters;
 
+import com.example.foodappproject.models.HomeVerModel;
+
+import java.util.ArrayList;
+
 public interface UpdateVerticalRec {
+    public void callBack(ArrayList<HomeVerModel> list, int position);
 }
