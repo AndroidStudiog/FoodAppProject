e9dff4220a9839779c080584d6e2bb72ba0eff54
AndroidStudiog <bitm-f19-098@superior.edu.pk>
2022-04-05 08:12:59 +0500

    project

--- app/src/main/java/com/example/foodappproject/RegistrationActivity.java
+++ app/src/main/java/com/example/foodappproject/RegistrationActivity.java
@@ -1,7 +1,9 @@
 package com.example.foodappproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -11,11 +13,21 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.foodappproject.database.DBHelper;
+import com.example.foodappproject.models.User;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class RegistrationActivity extends AppCompatActivity {
     EditText name,email,password;
     Button register;
     TextView register_sign_in;
+    FirebaseAuth auth;
+    FirebaseDatabase database;
+    DatabaseReference reference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,15 +37,45 @@ public class RegistrationActivity extends AppCompatActivity {
         email=(EditText) findViewById(R.id.txt_reg_email);
         password=(EditText)findViewById(R.id.txt_reg_password);
 
-        DBHelper helper=new DBHelper(this);
+        //     DBHelper helper=new DBHelper(this);
+
+        auth=FirebaseAuth.getInstance();
+        database= FirebaseDatabase.getInstance();
+
+        ProgressDialog dialog=new ProgressDialog(RegistrationActivity.this);
+        dialog.setTitle("Creating Account");
+        dialog.setMessage("We're creating your account.");
 
         register=(Button) findViewById(R.id.btn_register);
         register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String Name=name.getText().toString();
-                String Email=email.getText().toString();
-                String Password=password.getText().toString();
+                dialog.show();
+
+                String Name = name.getText().toString();
+                String Email = email.getText().toString();
+                String Password = password.getText().toString();
+
+                auth.createUserWithEmailAndPassword(Email, Password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            User user = new User(Name, Email, Password);
+
+                            String id = task.getResult().getUser().getUid();
+
+                            database.getReference().child("User").child(id).setValue(user);
+
+                            Toast.makeText(RegistrationActivity.this, "Account is Created.", Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(RegistrationActivity.this, LoginActivity.class);
+                            startActivity(intent);
+                        } else {
+                            Toast.makeText(RegistrationActivity.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                        dialog.dismiss();
+                    }
+                });
+                /*
                 if (Name.equals("") || Email.equals("") || Password.equals("")){
                     Toast.makeText(RegistrationActivity.this, "Please fill all Field.", Toast.LENGTH_SHORT).show();
                 }
@@ -54,14 +96,15 @@ public class RegistrationActivity extends AppCompatActivity {
                         Toast.makeText(RegistrationActivity.this, "Please choose another Email.", Toast.LENGTH_SHORT).show();
                     }
                 }
+                 */
             }
         });
 
-        register_sign_in=(TextView) findViewById(R.id.txt_register_sign_in);
+        register_sign_in = (TextView) findViewById(R.id.txt_register_sign_in);
         register_sign_in.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent=new Intent(RegistrationActivity.this,LoginActivity.class);
+                Intent intent = new Intent(RegistrationActivity.this, LoginActivity.class);
                 startActivity(intent);
             }
         });
