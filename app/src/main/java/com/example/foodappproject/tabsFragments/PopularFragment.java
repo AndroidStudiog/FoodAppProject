885c896bd094a0e4e656861aaf1384117aa039ee
sUnGi <bitm-f19-099@superior.edu.pk>
2022-04-08 12:02:34 +0500

    project

---
+++ app/src/main/java/com/example/foodappproject/tabsFragments/PopularFragment.java
@@ -0,0 +1,22 @@
+package com.example.foodappproject.tabsFragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.foodappproject.R;
+
+public class PopularFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view=inflater.inflate(R.layout.fragment_popular, container, false);
+        return  view;
+    }
+}
