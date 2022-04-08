15253a4431a3978f73d26c262358a9b2bd41a6f0
sUnGi <bitm-f19-099@superior.edu.pk>
2022-04-08 13:07:16 +0500

    project

--- app/src/main/java/com/example/foodappproject/ui/favourite/FavouriteFragment.java
+++ app/src/main/java/com/example/foodappproject/ui/favourite/FavouriteFragment.java
@@ -7,14 +7,32 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.viewpager.widget.ViewPager;
 
 import com.example.foodappproject.R;
+import com.example.foodappproject.adapters.FragmentAdapter;
+import com.google.android.material.tabs.TabLayout;
 
 public class FavouriteFragment extends Fragment {
+    TabLayout tab_layout;
+    ViewPager view_pager;
 
-    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_favourite, container, false);
 
-        View view= inflater.inflate(R.layout.fragment_favourite, container, false);
+        view_pager = (ViewPager) view.findViewById(R.id.viewPager);
+
+        FragmentManager fragmentManager = getParentFragmentManager();
+        FragmentAdapter adapter = new FragmentAdapter(fragmentManager);
+        view_pager.setAdapter(adapter);
+
+        tab_layout = (TabLayout) view.findViewById(R.id.tabLayout);
+        tab_layout.setupWithViewPager(view_pager);
+
         return view;
     }
 }
