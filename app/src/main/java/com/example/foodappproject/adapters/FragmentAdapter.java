54df4d18009cc84ba53b934e9033de658bc0ea4d
sUnGi <bitm-f19-099@superior.edu.pk>
2022-04-08 12:11:39 +0500

    project

--- app/src/main/java/com/example/foodappproject/adapters/FragmentAdapter.java
+++ app/src/main/java/com/example/foodappproject/adapters/FragmentAdapter.java
@@ -1,4 +1,54 @@
 package com.example.foodappproject.adapters;
 
-public class FragmentAdapter {
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import com.example.foodappproject.tabsFragments.FeaturedFragment;
+import com.example.foodappproject.tabsFragments.NewFragment;
+import com.example.foodappproject.tabsFragments.PopularFragment;
+
+public class FragmentAdapter extends FragmentPagerAdapter {
+
+
+    public FragmentAdapter(@NonNull FragmentManager fm) {
+        super(fm);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0:
+                return new FeaturedFragment();
+            case 1:
+                return new PopularFragment();
+            case 2:
+                return new NewFragment();
+            default:
+                return new FeaturedFragment();
+        }
+    }
+    @Override
+    public int getCount() {
+        return 3;
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        String title=null;
+        if (position==0){
+            title="FEATURED";
+        }
+        else if (position==1){
+            title="POPULAR";
+        }
+        else if (position==2){
+            title="NEW";
+        }
+        return title;
+    }
 }
