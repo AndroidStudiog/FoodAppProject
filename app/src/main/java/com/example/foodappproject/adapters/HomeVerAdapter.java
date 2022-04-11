3130436b29b8574b3798ba527259978cbc8d2bad
AndroidStudiog <bitm-f19-098@superior.edu.pk>
2022-04-11 23:22:37 +0500

    project

--- app/src/main/java/com/example/foodappproject/adapters/HomeVerAdapter.java
+++ app/src/main/java/com/example/foodappproject/adapters/HomeVerAdapter.java
@@ -4,14 +4,17 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.foodappproject.R;
 import com.example.foodappproject.models.HomeVerModel;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
 
 import java.util.ArrayList;
 
@@ -34,11 +37,55 @@ public class HomeVerAdapter extends RecyclerView.Adapter<HomeVerAdapter.ViewHold
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         HomeVerModel mdl=list.get(position);
+
+        final int mImage=mdl.getImage();
+        final String mName=mdl.getName();
+        final String mTiming=mdl.getTiming();
+        final String mRating=mdl.getRating();
+        final String mPrice=mdl.getPrice();
+
         holder.image.setImageResource(mdl.getImage());
         holder.text.setText(mdl.getName());
         holder.timing.setText(mdl.getTiming());
         holder.rating.setText(mdl.getRating());
         holder.price.setText(mdl.getPrice());
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            ImageView bottom_sheet_image;
+            TextView bottom_sheet_name,bottom_sheet_timing,bottom_sheet_rating,bottom_sheet_price;
+            Button bottom_sheet_add_to_cart;
+
+            @Override
+            public void onClick(View view) {
+                BottomSheetDialog bottomSheetDialog=new BottomSheetDialog(context,R.style.BottomSheetTheme);
+
+                View sheetView=LayoutInflater.from(context).inflate(R.layout.bottom_sheet_layout,null);
+
+                bottom_sheet_image=(ImageView) sheetView.findViewById(R.id.bottom_sheet_img);
+                bottom_sheet_name=(TextView) sheetView.findViewById(R.id.bottom_sheet_name);
+                bottom_sheet_timing=(TextView) sheetView.findViewById(R.id.bottom_sheet_time);
+                bottom_sheet_rating=(TextView) sheetView.findViewById(R.id.bottom_sheet_rating);
+                bottom_sheet_price=(TextView) sheetView.findViewById(R.id.bottom_sheet_price);
+
+                bottom_sheet_image.setImageResource(mImage);
+                bottom_sheet_name.setText(mName);
+                bottom_sheet_timing.setText(mTiming);
+                bottom_sheet_rating.setText(mRating);
+                bottom_sheet_price.setText(mPrice);
+
+                bottomSheetDialog.setContentView(sheetView);
+                bottomSheetDialog.show();
+
+                bottom_sheet_add_to_cart=(Button) sheetView.findViewById(R.id.bottom_sheet_add_to_cart);
+                bottom_sheet_add_to_cart.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Toast.makeText(context, "Added to a Cart.", Toast.LENGTH_SHORT).show();
+                        bottomSheetDialog.dismiss();
+                    }
+                });
+            }
+        });
     }
 
     @Override
