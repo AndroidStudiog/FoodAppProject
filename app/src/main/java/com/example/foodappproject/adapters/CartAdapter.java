f38e711a13f1784f681df7675e704198a4dcecb5
AndroidStudiog <bitm-f19-098@superior.edu.pk>
2022-04-11 22:51:30 +0500

    project

--- app/src/main/java/com/example/foodappproject/adapters/CartAdapter.java
+++ app/src/main/java/com/example/foodappproject/adapters/CartAdapter.java
@@ -1,4 +1,60 @@
 package com.example.foodappproject.adapters;
 
-public class CartAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.foodappproject.R;
+import com.example.foodappproject.models.CartModel;
+
+import java.util.ArrayList;
+
+public class CartAdapter extends RecyclerView.Adapter<CartAdapter.ViewHolder> {
+    ArrayList<CartModel> list;
+    Context context;
+
+    public CartAdapter(ArrayList<CartModel> list, Context context) {
+        this.list = list;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view= LayoutInflater.from(context).inflate(R.layout.mycart_item,parent,false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        CartModel mdl=list.get(position);
+        holder.image.setImageResource(mdl.getImage());
+        holder.name.setText(mdl.getName());
+        holder.rating.setText(mdl.getRating());
+        holder.timing.setText(mdl.getPrice());
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        ImageView image;
+        TextView name,rating,timing;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            image=(ImageView) itemView.findViewById(R.id.cart_img);
+            name=(TextView) itemView.findViewById(R.id.cart_name);
+            rating=(TextView) itemView.findViewById(R.id.cart_rating);
+            timing=(TextView) itemView.findViewById(R.id.cart_price);
+        }
+    }
 }
