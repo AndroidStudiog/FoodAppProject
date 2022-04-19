f19859ebead0c6a7e45e15308949a43270e7a2b7
AndroidStudiog <bitm-f19-098@superior.edu.pk>
2022-04-19 20:23:05 +0500

    project

--- app/src/main/java/com/example/foodappproject/ui/favourite/FavouriteFragment.java
+++ app/src/main/java/com/example/foodappproject/ui/favourite/FavouriteFragment.java
@@ -1,20 +1,24 @@
 package com.example.foodappproject.ui.favourite;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.viewpager.widget.ViewPager;
 
+import com.example.foodappproject.MainActivity;
 import com.example.foodappproject.R;
 import com.example.foodappproject.adapters.FragmentAdapter;
 import com.google.android.material.tabs.TabLayout;
 
 public class FavouriteFragment extends Fragment {
+    TextView more;
     TabLayout tab_layout;
     ViewPager view_pager;
 
@@ -23,6 +27,16 @@ public class FavouriteFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_favourite, container, false);
+
+        more=(TextView) view.findViewById(R.id.viewMore);
+
+        more.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getContext(), MainActivity.class);
+                getContext().startActivity(intent);
+            }
+        });
 
         view_pager = (ViewPager) view.findViewById(R.id.viewPager);
 
