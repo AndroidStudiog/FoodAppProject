6f4ce299a221453814b0bdf6280ffe83a7a2c550
AndroidStudiog <bitm-f19-098@superior.edu.pk>
2022-05-05 03:02:01 +0500

    project

--- app/src/main/java/com/example/foodappproject/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/foodappproject/ui/home/HomeFragment.java
@@ -7,6 +7,7 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.widget.SearchView;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -21,21 +22,49 @@ import com.example.foodappproject.models.HomeHorModel;
 import com.example.foodappproject.models.HomeVerModel;
 
 import java.util.ArrayList;
+import java.util.Locale;
 
 public class HomeFragment extends Fragment implements UpdateVerticalRec {
-    RecyclerView hor_RecycleVew, ver_RecycleVew;
+    RecyclerView hor_RecycleVew,ver_RecycleVew;
+    TextView name;
+    SearchView searchView;
+    ArrayList<HomeHorModel> list1;
+    HomeHorAdapter adapter1;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        View view=inflater.inflate(R.layout.fragment_home, container, false);
+
+        searchView=(SearchView) view.findViewById(R.id.search_view);
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                filter(newText);
+                return true;
+            }
+        });
+
+       /*
+        name=(TextView) view.findViewById(R.id.name);
+
+        Intent intent=getActivity().getIntent();
+        String mail=intent.getStringExtra("email");
+
+        name.setText(mail);
+        */
 
         //////////Horizontal RecycleView
 
         hor_RecycleVew=(RecyclerView) view.findViewById(R.id.home_hor_rec);
 
-        ArrayList<HomeHorModel> list1=new ArrayList<>();
+        list1=new ArrayList<>();
 
         list1.add(new HomeHorModel(R.drawable.c_pizza,"Pizza"));
         list1.add(new HomeHorModel(R.drawable.c_burger,"Burger"));
@@ -43,7 +72,7 @@ public class HomeFragment extends Fragment implements UpdateVerticalRec {
         list1.add(new HomeHorModel(R.drawable.c_ice_cream,"Ice Cream"));
         list1.add(new HomeHorModel(R.drawable.c_sandwich,"Sandwich"));
 
-        HomeHorAdapter adapter1=new HomeHorAdapter(list1,getActivity(),this);
+        adapter1=new HomeHorAdapter(list1,getActivity(),this);
         hor_RecycleVew.setAdapter(adapter1);
 
         LinearLayoutManager linearLayoutManager1=new LinearLayoutManager(getContext(),LinearLayoutManager.HORIZONTAL,false);
@@ -62,6 +91,16 @@ public class HomeFragment extends Fragment implements UpdateVerticalRec {
         ver_RecycleVew.setLayoutManager(linearLayoutManager2);
 
         return view;
+    }
+
+    private void filter(String newText) {
+        ArrayList<HomeHorModel> list3=new ArrayList<>();
+        for (HomeHorModel item: list1){
+            if(item.getName().toLowerCase(Locale.ROOT).contains(newText.toLowerCase(Locale.ROOT))){
+                list3.add(item);
+            }
+        }
+        adapter1.searchBox(list3);
     }
 
     @Override
