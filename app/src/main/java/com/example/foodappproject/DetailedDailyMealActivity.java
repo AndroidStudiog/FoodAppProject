d6b0ed0fba8be90161dd0ecc8e76427537306ad0
AndroidStudiog <bitm-f19-098@superior.edu.pk>
2022-05-08 12:51:53 +0500

    project

--- app/src/main/java/com/example/foodappproject/DetailedDailyMealActivity.java
+++ app/src/main/java/com/example/foodappproject/DetailedDailyMealActivity.java
@@ -21,7 +21,6 @@ public class DetailedDailyMealActivity extends AppCompatActivity {
     ImageView image;
     String type;
     FloatingActionButton carts;
-    //
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
