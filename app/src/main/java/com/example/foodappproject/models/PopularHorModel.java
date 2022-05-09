66541068a258959ddd64e56fd7cdb4a4f7d66ee4
sUnGi <bitm-f19-099@superior.edu.pk>
2022-05-09 10:18:16 +0500

    project

--- app/src/main/java/com/example/foodappproject/models/PopularHorModel.java
+++ app/src/main/java/com/example/foodappproject/models/PopularHorModel.java
@@ -23,7 +23,9 @@ public class PopularHorModel {
         return description;
     }
 
-
+    public void setDescription(String description) {
+        this.description = description;
+    }
 
     public String getName() {
         return name;
