ec0c53bdda2c637295c74d24ceedec9a07cc6bab
munsif <munsifshahzad1234@gmail.com>
2022-05-12 23:49:50 +0500

    project

--- app/src/main/java/com/example/foodappproject/ui/dailyMeal/DailyMealFragment.java
+++ app/src/main/java/com/example/foodappproject/ui/dailyMeal/DailyMealFragment.java
@@ -33,7 +33,7 @@ public class DailyMealFragment extends Fragment {
         DailyMealAdapter adapter=new DailyMealAdapter(list,getContext());
         dailyMeal_recyclerView.setAdapter(adapter);
 
-        LiLayoutManager linearLayoutManager=new LinearLayoutManager(getContext());
+        LinearLayoutManager linearLayoutManager=new LinearLayoutManager(getContext());
         dailyMeal_recyclerView.setLayoutManager(linearLayoutManager);
 
         return view;
